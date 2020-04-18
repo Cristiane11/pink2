7351602fc499ab71a1d975dcb644def616f8d32e
Cristiane11 <bjwmcd@yahoo.com>
2020-04-17 21:26:42 -0400

    update

--- pink/src/components/ContactCard.js
+++ pink/src/components/ContactCard.js
@@ -3,8 +3,11 @@ import ReactDom from 'react-dom'
 
 function ContactCard(){
     return(
-        <div>
-
+        <div className="contact-card">
+            <img src="http://placekitten.com/400/300"/>
+            <h3>Destroyer</h3>
+            <p>Phone: (212) 555-3456</p>
+            <p>Email: ofworlds@yahoo.com</p>
         </div>
     )
 }
