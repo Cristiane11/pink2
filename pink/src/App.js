fa284a6a891e030b42ab7daa9251865bce94d861
Cristiane11 <bjwmcd@yahoo.com>
2020-04-18 00:05:33 -0400

    update

--- pink/src/App.js
+++ pink/src/App.js
@@ -17,7 +17,9 @@ function App() {
         <ToDoList/>
         <ToDoList/>
         <ToDoList/>
-        <ContactCard/>S
+        <ContactCard name="Mr. Whiskerson" imgUrl="http://placekitten.com/300/200" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow"/>
+        <ContactCard name="Cristiane Abreu" imgUrl="http://placekitten.com/400/300" phone="(212) 777-2020" email="cristiane.abreu@martindale.com" />
+
        
       </header>
     </div>
